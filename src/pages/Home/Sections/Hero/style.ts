f66dd4c8369285e28c79b5aa4fdcac5f5d882475
caio20538 <caio20538@gmail.com>
2025-08-05 14:21:20 -0300

    fix: Hero component UI

--- src/pages/Home/Sections/Hero/style.ts
+++ src/pages/Home/Sections/Hero/style.ts
@@ -4,12 +4,21 @@ export const StyledHero = styled("div")( ({theme}) => ({
     backgroundColor: theme.palette.primary.main,
     height: "100vh",
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+
+    //aqui é para o mobile preciso colocar os 2 pq as mudanças são aplicadaspara todos
+    [theme.breakpoints.up('xs')]: { // <= mobile
+        paddingTop: "100px"
+    },
+
+    [theme.breakpoints.up('md')]: { // >= mobile
+        paddingTop: "0"
+    },
 }));
 
 export const StyledImg = styled("img")(({theme}) => ({
-    width: "80%",
-    height:"80%",
+    width: "75%",
+    height:"75%",
     borderRadius: "50%",
     border: `2px solid ${theme.palette.primary.contrastText}`
 }));
