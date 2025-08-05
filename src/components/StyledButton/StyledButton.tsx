88cbcfc63c9464b9b3a0d78e895d2d2b910322f5
caio20538 <caio20538@gmail.com>
2025-08-05 14:26:03 -0300

    feat: add onClick at StyledButton component

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -2,14 +2,15 @@ import type { ReactNode } from "react"
 import * as S from "./style"
 
 interface StyledButtonProps{
-  children: ReactNode
+  children: ReactNode,
+  onClick: () => void
 }
 
-export const StyledButton:React.FC<StyledButtonProps> = ({children}) => {
+export const StyledButton:React.FC<StyledButtonProps> = ({children, onClick}) => {
 
     
   return (
-    <S.StyledButton>
+    <S.StyledButton onClick={onClick}>
       {children}
     </S.StyledButton>
     
